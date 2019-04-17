dce0f14cc8bea4953990909f7427f1c7cc6b8f4e
ravitejag4u <ravi7.gadde@gmail.com>
2019-04-16 22:34:11 -0500

    More notes to test interfaces

--- src/main/java/com/java/api/oops/concepts/AbstractTestClass.java
+++ src/main/java/com/java/api/oops/concepts/AbstractTestClass.java
@@ -1,13 +1,18 @@
 package com.java.api.oops.concepts;
 
 //Abstract classes can only have default or public modifiers
-public abstract class AbstractTestClass implements TestInterface{
-	
+public abstract class AbstractTestClass implements TestInterface, ParentTestInterface {
+
 	public abstract void abstractMethod();
-	
-	public void regularMethod () {
+
+	public void regularMethod() {
+
+		// Since testVariable (i.e static) is both in TestInterface,
+		// ParentTestInterface, It has to be accessed with Interface name
+		// int x = testVariable;
 		
-		int x = testVariable;
+		int x = TestInterface.testVariable;
 		
+		int y = ParentTestInterface.testVariable;
 	}
 }
